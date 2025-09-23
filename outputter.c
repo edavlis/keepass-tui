15ab649a453fefb2316dcacd53f7e234d320d787
ilvades <linuxbenemail@gmail.com>
2025-09-23 21:23:22 +0100

    Make outputter flush stdout

--- outputter.c
+++ outputter.c
@@ -3,9 +3,12 @@
 
 
 int main() {
+  while (1) {
   char write_to_me[100] = "";
   while (strcmp(write_to_me , "STOP") != 0) {
     scanf(" %[^\n]", write_to_me);
-    printf("%s\n", write_to_me);
+    printf("Output: %s\n", write_to_me);
+    fflush(stdout);
   }
+}
 }
