59f816268605a493f5c1dfaeb7f84de9d9676009
ilvades <linuxbenemail@gmail.com>
2025-09-23 21:22:35 +0100

    Added read from keyboard functionality, fixed dup2 misconfiguration

--- keepass-cli.c
+++ keepass-cli.c
@@ -31,34 +31,48 @@ int main(int argc, char *argv[]) {
       close(in_to_child[1]);
       close(out_of_child[0]);
 
-      dup2(out_of_child[1], STDIN_FILENO);
-      dup2(out_of_child[1], STDERR_FILENO);
-      dup2(in_to_child[0], STDIN_FILENO);
+      dup2(out_of_child[1], STDOUT_FILENO); // redirect child STDOUT with out of child pipe
+      dup2(out_of_child[1], STDERR_FILENO); // redirect child STDOUT with out of child pipe
+      dup2(in_to_child[0], STDIN_FILENO); // CHILD STDINPUT now comes froem the intochild pipe
 
     
-      execlp("./outputter","./outputter");
+      execlp("./outputter","./outputter", NULL);
       close(in_to_child[0]);
       close(out_of_child[1]);
     } else { // parent
 
+      // close file descriptors
       close(in_to_child[0]);
       close(out_of_child[1]);
 
+      // make buffers to write to
       char read_buffer[64] = "";
-      char buffer[64] = "Hello!\n";
+      char buffer[64] = "";
 
-      
+      //
+
+      while (1) {
+      // get ketboard input
+      printf("Enter stuff to be inputted: ");
+      fgets(buffer, sizeof(buffer), stdin);
+
       if (write(in_to_child[1], buffer, sizeof(buffer)) == -1) {
         printf("Failed to write to child write pipe");
       };
 
-
-      if (read(out_of_child[0],read_buffer,sizeof(read_buffer)) == -1) {
-        printf("Failed to read from child read pipe");
-      } else if (read(out_of_child[0],read_buffer,sizeof(read_buffer)) == EOF) {
+      // read from the output of keepassxc-cli
+      int size_read = read(out_of_child[0],read_buffer,sizeof(read_buffer)); 
+      if (size_read == -1) {
+      printf("Failed to read from child read pipe");
+      }
+       if (size_read == 0) {
         printf("Reached end of input stream");
       }
-
+      
+       // print output
+      printf("you inputted %s", buffer);
+      // clsoe file descriptors
+      }
      close(in_to_child[1]);
      close(out_of_child[0]);
 
