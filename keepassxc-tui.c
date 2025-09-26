c3eee04e53ff792d4fa791faa07d7ea847b5e90a
ilvades <linuxbenemail@gmail.com>
2025-09-26 23:33:48 +0100

    ls

--- keepassxc-tui.c
+++ keepassxc-tui.c
@@ -1,11 +1,41 @@
 #include <stdio.h>
 #include <termios.h>
 #include <poll.h>
-#include <stdlib.h>
+#include <sys/ioctl.h>
 #include <string.h>
 #include <sys/poll.h>
 #include <unistd.h>
 
+// termios functions
+void enable_raw_mode(struct termios *new_terminal_settings) {
+  new_terminal_settings->c_lflag &= ~( ICANON | ECHO);
+  tcsetattr(STDIN_FILENO,TCSANOW, new_terminal_settings);
+}
+
+void reset_terminal(struct termios *old_terminal_settings) {
+  tcsetattr(STDIN_FILENO,TCSANOW,old_terminal_settings);
+}
+
+void get_terminal_dimensions( int *width, int *height) {
+   
+  struct winsize windowSize;
+  ioctl(STDIN_FILENO,TIOCGWINSZ, &windowSize);
+
+  *width = windowSize.ws_col;
+  *height = windowSize.ws_row;
+
+}
+
+void moveCursor(int x, int y) {
+  printf("\033[%d;%dH", y, x);
+}
+
+void clearscreen() {
+  printf("\033[2J");
+    
+}
+
+// input output functions
 int  readFromFileDescriptor(int filedescriptor) {
   char buffer[4095] = "";
   int amountRead = read(filedescriptor, buffer,4095 -1);
@@ -30,12 +60,24 @@ int  readFromFileDescriptor(int filedescriptor) {
 
 
 int main(int argc, char *argv[]) {
-
-  // usage
+  
+  // usage, refactor later
   if (argc < 2) {
     printf("Usage: keepassxc-tui <path-to-database>\n");
     return 1;
   }
+
+  //termios stuff
+  // get terminal dimentsions
+    int height_terminal;
+    int width_terminal;
+    get_terminal_dimensions(&width_terminal, &height_terminal);
+  // get new and old terminal settings
+    struct termios new_terminal_settings, old_terminal_settings; 
+    tcgetattr(STDIN_FILENO, &new_terminal_settings);
+    tcgetattr(STDIN_FILENO, &old_terminal_settings);
+    enable_raw_mode(&new_terminal_settings);
+  
   
   // create pipes
     int in_to_child[2];
@@ -67,7 +109,9 @@ int main(int argc, char *argv[]) {
       execlp("keepassxc-cli","keepassxc-cli", "open" , argv[1] , NULL);
       close(in_to_child[0]);
       close(out_of_child[1]);
-    } else { // parent
+    }  
+
+      moveCursor((width_terminal / 2), (height_terminal / 2));
 
         struct pollfd STDIN_CHILD_filedescriptors[2]; // 0  is child 1 is stdin
 
@@ -84,53 +128,33 @@ int main(int argc, char *argv[]) {
 
       // make buffers to write to
       char buffer[64*64] = "";
-      do {
-        
-      int CHILDOUT_ready=poll(STDIN_CHILD_filedescriptors, 2, -1);
-      if (CHILDOUT_ready == -1) {
-        printf("poll failed");
+      while (1) {
+        moveCursor((width_terminal / 2), (height_terminal / 2));
+        clearscreen();
+
+        int CHILDOUT_ready=poll(STDIN_CHILD_filedescriptors, 2, -1);
+        if (CHILDOUT_ready == -1) {
+          printf("poll failed");
+        }
+
+        if (STDIN_CHILD_filedescriptors[0].revents & POLLIN) {
+        readFromFileDescriptor(out_of_child[0]);
+        fflush(stdout);
+        }
+
+        // get ketboard input
+        if (STDIN_CHILD_filedescriptors[1].revents & POLLIN) {
+          reset_terminal(&old_terminal_settings);
+        fgets(buffer, sizeof(buffer), stdin);
+        if (write(in_to_child[1], buffer, strlen(buffer)) == -1) {
+          printf("Failed to write to child write pipe");
+        };
+        }
+
+
+        } 
+
+       close(in_to_child[1]);
+       close(out_of_child[0]);
+
       }
-
-      if (STDIN_CHILD_filedescriptors[0].revents & POLLIN) {
-      readFromFileDescriptor(out_of_child[0]);
-      fflush(stdout);
-      }
-
-      // get ketboard input
-      if (STDIN_CHILD_filedescriptors[1].revents & POLLIN) {
-      fgets(buffer, sizeof(buffer), stdin);
-      if (write(in_to_child[1], buffer, strlen(buffer)) == -1) {
-        printf("Failed to write to child write pipe");
-      };
-      }
-
-
-      } while (1);
-
-     close(in_to_child[1]);
-     close(out_of_child[0]);
-
-      }
-      
-    }
-
-
-
-
-
-
-
-
-      //// read from the output of keepassxc-cli
-      //int size_read = read(out_of_child[0],read_buffer,sizeof(read_buffer) -1); 
-      //if (size_read > 0) {
-      //  read_buffer[size_read] = '\0';
-      //printf("%s", read_buffer);
-      //fflush(stdout);
-      //}
-      //if (size_read == -1) {
-      //printf("Failed to read from child read pipe");
-      //}
-      //if (size_read == 0) {
-      //  printf("Reached end of input stream");
-      //}
